c57f4bc94fb12587bfe8fa7eadcc7957acc48793
Musweta <kelvin.musweta@strathmore.edu>
2025-07-17 19:43:31 +0300

    This is up to date to sprint 6

---
+++ dbConnect.php
@@ -0,0 +1,16 @@
+<?php
+date_default_timezone_set('Africa/Nairobi');
+$servername = "localhost";
+$username = "root";
+$password = "alex";
+$dbname = "sect";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    // echo "Connected successfully to the database {$dbname}.";
+}
